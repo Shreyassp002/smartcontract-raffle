ffbeef9dd6e251150ea0aebbf5d08cc2df8d65f3
Shreyas Patil <shreyassp002@gmail.com>
2025-07-09 19:34:35 +0530

    Imported and overriden VRFConsumerBaseV2 contract

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity 0.8.7;
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 
 error Raffle_NotEnoughETHEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     /* State Variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
@@ -11,7 +13,10 @@ contract Raffle {
     /* Events */
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 enteranceFee) {
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 enteranceFee
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = enteranceFee;
     }
 
@@ -23,13 +28,19 @@ contract Raffle {
         emit RaffleEnter(msg.sender);
     }
 
-    // function pickRandomWinner() {}
+    function requestRandomWinner() external {}
 
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
+
+    /* View / Pure functions */
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
 
-    function getPlayer(uint256 index) public view returns (address){
+    function getPlayer(uint256 index) public view returns (address) {
         return s_players[index];
     }
 }
