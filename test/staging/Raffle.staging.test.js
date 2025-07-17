a9d0dc31e34494798df04aac2a36a67f504e9d32
Shreyas Patil <shreyassp002@gmail.com>
2025-07-17 11:55:43 +0530

    Added staging test

---
+++ test/staging/Raffle.staging.test.js
@@ -0,0 +1,69 @@
+const { assert, expect } = require("chai")
+const { getNamedAccounts, ethers, network } = require("hardhat")
+const { developmentChains } = require("../../helper-hardhat-config")
+
+developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Raffle Staging Tests", function () {
+          let raffle, raffleEntranceFee, deployer
+
+          beforeEach(async function () {
+              deployer = (await getNamedAccounts()).deployer
+              raffle = await ethers.getContract("Raffle", deployer)
+              raffleEntranceFee = await raffle.getEntranceFee()
+          })
+
+          describe("fulfillRandomWords", function () {
+              it("works with live Chainlink Keepers and Chainlink VRF, we get a random winner", async function () {
+                  // enter the raffle
+                  console.log("Setting up test...")
+                  const startingTimeStamp = await raffle.getLatestTimeStamp()
+                  const accounts = await ethers.getSigners()
+
+                  console.log("Setting up Listener...")
+                  await new Promise(async (resolve, reject) => {
+                      // setup listener before we enter the raffle
+                      // Just in case the blockchain moves REALLY fast
+                      raffle.once("WinnerPicked", async () => {
+                          console.log("WinnerPicked event fired!")
+                          try {
+                              // add our asserts here
+                              const recentWinner = await raffle.getRecentWinner()
+                              const raffleState = await raffle.getRaffleState()
+                              const winnerEndingBalance = await ethers.provider.getBalance(accounts[0].address)
+                              const endingTimeStamp = await raffle.getLatestTimeStamp()
+
+                              // Modern Chai matcher for reverted transactions
+                              await expect(raffle.getPlayer(0)).to.be.reverted
+                              
+                              // Use .equal() instead of .toString() comparisons
+                              assert.equal(recentWinner, accounts[0].address)
+                              assert.equal(raffleState, 0)
+                              
+                              // Use BigInt arithmetic for balance calculations
+                              assert.equal(
+                                  winnerEndingBalance.toString(),
+                                  (winnerStartingBalance + raffleEntranceFee).toString()
+                              )
+                              assert(endingTimeStamp > startingTimeStamp)
+                              resolve()
+                          } catch (error) {
+                              console.log(error)
+                              reject(error)
+                          }
+                      })
+                      
+                      // Get starting balance BEFORE entering the raffle
+                      const winnerStartingBalance = await ethers.provider.getBalance(accounts[0].address)
+                      
+                      // Then entering the raffle
+                      console.log("Entering Raffle...")
+                      const tx = await raffle.enterRaffle({ value: raffleEntranceFee })
+                      await tx.wait(1)
+                      console.log("Ok, time to wait...")
+
+                      // and this code WONT complete until our listener has finished listening!
+                  })
+              })
+          })
+      })
