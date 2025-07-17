452c1a900b24840b656016a368d8928678fabb2b
Shreyas Patil <shreyassp002@gmail.com>
2025-07-17 17:49:19 +0530

    Reduced callbackGasLimit value

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -6,8 +6,9 @@ const networkConfig = {
         vrfCoordinatorV2Plus: "0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B", // Updated to v2.5 coordinator
         entranceFee: ethers.parseEther("0.01"),
         gasLane: "0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae", // Updated gas lane for v2.5
-        subscriptionId: "38138072735821051284414751774607864477651026379369448029996162176052472611692", // You'll need to create a new v2.5 subscription
-        callbackGasLimit: "2500000",
+        subscriptionId:
+            "54557647894630960898604562076359003379284596268715729073031521891914113887506", // You'll need to create a new v2.5 subscription
+        callbackGasLimit: "300000",
         interval: "30",
     },
     31337: {
